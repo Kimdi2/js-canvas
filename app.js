2e6bfcdcd2e754c53d495df01870e5d922cdb8a4
bigperson <daein7076@naver.com>
2022-08-02 23:47:48 +0900

    (2.2) line width

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const canvas = document.querySelector("canvas");
 // ctx = context
 const ctx = canvas.getContext("2d");
+const lineWidth = document.getElementById("line-width");
 
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
 function onMove(event) {
   if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
     return;
   }
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -22,9 +23,16 @@ function startPainting() {
 }
 function finishPainting() {
   isPainting = false;
+  ctx.stroke();
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", finishPainting);
 canvas.addEventListener("mouseleave", finishPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
